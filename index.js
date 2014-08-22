b8b8435295bdfc3fe152fad0370a6e145cda1ed5
Alex <alex@nanopay.net>
2014-08-22 11:52:36 -0400

    Refactored code for simplicity

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
+/**
+ * Loopback console tasks
+ *
+ * @company Nanopay inc.
+ * @year 2014
+ * @author Alex Strutsynskyi cajoy.dev@gmail.com
+ */
 
 var _defaults = require('lodash.defaults');
 
@@ -9,5 +16,25 @@ function consoleTasks(app, options) {
     'tasksPath': require('path').resolve() + '/tasks/'
   });
 
-  return require('./lib/console')(app, options);
+  var cls = {};
+
+  cls.intercept = function () {
+    var argv = require('minimist')(process.argv.slice(2));
+
+    if (argv.task) {
+      console.log('Welcome to console!');
+
+      if (fs.existsSync(options.tasksPath + argv.task + '.js')) {
+        console.log('Executing "' + argv.task + '":');
+
+        return require(options.tasksPath + argv.task)(app, argv);
+      } else {
+        console.log('Task "' + argv.task + '" not found');
+
+        return process.exit(0);
+      }
+    }
+  }
+
+  return cls;
 }
